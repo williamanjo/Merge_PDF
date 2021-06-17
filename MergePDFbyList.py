d9c468005a0151b050bd263368649e75a6664798
William Silva <william.anjodaguarda20@gmail.com>
2021-06-17 17:25:17 -0300

    Update MergePDFbyList.py

--- MergePDFbyList.py
+++ MergePDFbyList.py
@@ -1,5 +1,6 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader
 import os
+from sys import exit
 from tkinter import filedialog , messagebox
 from tkinter import *
 
@@ -11,9 +12,14 @@ root.withdraw()
 
 while(True):
     Directory = filedialog.askdirectory(title ="Selecione a Pasta Que Contêm Os Arquivos (*.pdf)")
-    if(messagebox.askokcancel(title="Pasta Que Contêm Os Arquivos (*.pdf)", message=Directory)):
-        break
-
+    if(Directory):
+        if(messagebox.askokcancel(title="Pasta Que Contêm Os Arquivos (*.pdf)", message=Directory)):
+                break
+        else:
+            if(messagebox.askquestion (title="Atenção", message="Fechar o Programa ? " ,icon = 'warning') == "yes"):
+                exit(0)
+    else:
+        exit(0)
 
 for dirpath, dirnames, filenames in os.walk(Directory):
     for filename in [f for f in filenames if (f.endswith(".pdf") or f.endswith(".PDF")) ]:
@@ -25,10 +31,16 @@ for i in all_files :
 # Loop nos nomes dos arquivos
 while(True):
     DirectorySaved = filedialog.askdirectory(title ="Selecione a Pasta Para salvar os Arquivos (*.pdf)")
-    if(messagebox.askokcancel(title="Pasta Para salvar os Arquivos (*.pdf)", message=DirectorySaved)):
-        break
+    if(DirectorySaved):
+        if(messagebox.askokcancel(title="Pasta Para salvar os Arquivos (*.pdf)", message=DirectorySaved)):
+            break
+        else:
+            if(messagebox.askquestion (title="Atenção", message="Fechar o Programa ? " ,icon = 'warning') == "yes"):
+                exit(0)
+    else:
+        exit(0)
     
-if(messagebox.askquestion (title="", message="Carregar de : " + Directory +"\n" + "Salvar Em : " + DirectorySaved,icon = 'warning') == "yes"):
+if(messagebox.askquestion (title="Atenção", message="Carregar de : " + Directory +"\n" + "Salvar Em : " + DirectorySaved,icon = 'warning') == "yes"):
     for pdfName in filesNames:
         mergedObject = PdfFileMerger() # Call the PdfFileMerger
 
@@ -44,4 +56,5 @@ if(messagebox.askquestion (title="", message="Carregar de : " + Directory +"\n" 
             mergedObject.write(DirectorySaved +"\\" + pdfName + ".pdf")
             # Limpando a referencia do objeto
             mergedObject = None
-            
+            exit(0)
+exit(0)
